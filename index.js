a4fa0a7ed92e71c2068fa8b262b10981865159b1
Brett Fuller <bfuller123@gmail.com>
2018-06-06 22:46:04 -0500

    removed unnecessary require statement

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const app = express();
 const chat = require("./controllers/ChatController");
 const main = require("./controllers/MainPageController");
 const http = require("http").Server(app);
-// const io = require("socket.io")(http);
 
 app.use(express.static("public"));
 app.use("/main", main);
